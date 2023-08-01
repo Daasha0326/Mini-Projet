4bc1dba229b9f57b9a4da0c6e00bedbfe6110c35
Daasha0326 <124213056+Daasha0326@users.noreply.github.com>
2023-08-01 08:35:12 +0000

    Update index.ts

--- interface Trans/src/index.ts
+++ interface Trans/src/index.ts
@@ -1,6 +1,36 @@
-let bonjour:string= "coucou";
+const compdteEdit: HTMLInputElement = document.querySelector("#inputCompteE");
+const nomEdit: HTMLInputElement = document.querySelector("#inputNomE");
+const montant: HTMLInputElement = document.querySelector("#inputMontant");
+const fournisseur: HTMLInputElement = document.querySelector("#idFrs");
+const transfert: HTMLInputElement = document.querySelector("#idTrs");
+const compdteDest: HTMLInputElement = document.querySelector("#inputCompteD");
+const nomDest: HTMLInputElement = document.querySelector("#inputNomD");
+const bouton: HTMLElement = document.querySelector(".button");
+const expediteur: HTMLElement = document.querySelector(".expedit");
+const as = document.querySelectorAll(".a");
 
-let tab:number[]= [1,2,3];
+let chemin: string = "http://127.0.0.1:8000/api/transactions/3";
 
-console.log(tab);
-// console.log(bonjour);
+console.log('coucou')
+
+bouton.addEventListener('click', () => {
+    fetch(chemin, {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            montant: montant.value,
+        })
+    })
+
+        .then(res => res.json())
+        .then((data) => {
+            console.log(data);
+        })
+
+    console.log("coucou");
+
+
+})
